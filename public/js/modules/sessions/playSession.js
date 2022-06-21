82315756151799a5d0c9d01200c123f635082d19
timmit147 <timmeeuwsen@hotmail.nl>
2022-06-21 15:11:23 +0200

    play bug fix

--- public/js/modules/sessions/playSession.js
+++ public/js/modules/sessions/playSession.js
@@ -20,10 +20,10 @@ export const autoPlay = async (sessionID) => {
   play = !play
   for (let i = await counter.step; i <= 100; i++) {
     while (play === true) {
-      fetchDataFromAPI('POST', `https://bubble-machine-api-dummy.herokuapp.com/rest/session/${sessionID}/step`)
+      console.log(await fetchDataFromAPI('POST', `https://bubble-machine-api-dummy.herokuapp.com/rest/session/${sessionID}/step`));
       const data = await fetchDataFromAPI('GET', `https://bubble-machine-api-dummy.herokuapp.com/rest/session/${sessionID}`)
       updateGraph(await data)
-      await wait(1000)
+      await wait(2000)
     }
   }
 }
